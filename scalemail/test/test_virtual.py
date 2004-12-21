04e5f042c4a27a2338c37c9ea87cf7feef600a82
tv <tv@b5f3cfec-f4e5-0310-a79a-9a52b1b86675>
2004-12-21 12:14:57 +0000

    Unit test for address extension passthrough in virtual map.
    git-svn-id: svn+ssh://open.inoi.fi/open-scalemail/trunk@51 b5f3cfec-f4e5-0310-a79a-9a52b1b86675

--- scalemail/test/test_virtual.py
+++ scalemail/test/test_virtual.py
@@ -167,6 +167,11 @@ scaleMailAlias: numbers@example.com
         r = ldaptestutil.pumpingDeferredResult(d)
         self.failUnlessIdentical(r, None)
 
+    def test_extension_simple(self):
+        d = self.map.get('foo+some.extension.here@example.com')
+        r = ldaptestutil.pumpingDeferredResult(d)
+        self.assertEquals(r, 'foo+some.extension.here@h1.scalemail.example.com')
+
 class Alias(SetupMixin, unittest.TestCase):
     ldif = """version: 1
 dn: dc=example,dc=com
