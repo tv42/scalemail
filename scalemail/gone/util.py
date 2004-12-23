6599edae13c984e99e2223b78bd09ee072a5f015
tv <tv@b5f3cfec-f4e5-0310-a79a-9a52b1b86675>
2004-12-23 10:30:54 +0000

    Add scalemail.gone.util.setSender().
    git-svn-id: svn+ssh://open.inoi.fi/open-scalemail/trunk@68 b5f3cfec-f4e5-0310-a79a-9a52b1b86675

--- scalemail/gone/util.py
+++ scalemail/gone/util.py
@@ -4,8 +4,12 @@ from scalemail.gone import igone
 
 def getSender(msg):
     from_ = msg.get_unixfrom()
+    assert from_ is not None
     assert from_.startswith('From ')
     return from_[len('From '):]
+
+def setSender(msg, sender):
+    msg.set_unixfrom('From %s' % sender)
 
 class RealWorld(object):
     implements(igone.IRealWorld)
