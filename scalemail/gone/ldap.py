a5da669ce62a20275ad8ffab4abb72f79f4ee1ef
tv <tv@b5f3cfec-f4e5-0310-a79a-9a52b1b86675>
2004-12-23 10:28:18 +0000

    Cleanup: rename scalemail.gone.ldap.is_active arguments.
    git-svn-id: svn+ssh://open.inoi.fi/open-scalemail/trunk@67 b5f3cfec-f4e5-0310-a79a-9a52b1b86675

--- scalemail/gone/ldap.py
+++ scalemail/gone/ldap.py
@@ -1,8 +1,8 @@
 from scalemail.gone import parse, times, respond
 
-def is_active(cfg, e, now):
+def is_active(config, entry, now):
     l = []
-    for s in e.get(cfg.getLDAPAttributeAway(), []):
+    for s in entry.get(config.getLDAPAttributeAway(), []):
         gone = parse.parse(s)
         l.append(gone)
     return times.findTime(now, l)
